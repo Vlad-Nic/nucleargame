8d4172d9dba9af2c965006ef923fd2e188c711d5
Nicholas Alexander Meert <alexmeert13@gmail.com>
2026-02-22 17:28:13 -0500

    Added Timer

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -23,13 +23,6 @@ public partial class Game : Node3D
 		_defaultPosition = PlayerCam.Position;
 		_targetPosition = PlayerCam.Position;
 		_targetRotation = new Vector3(0,0,0);
-		var timer = GetNode<Timer>("FnafTimer");
-		timer.Timeout += OnTimerTimeout;
-	}
-	
-	private void OnTimerTimeout()
-	{
-		GetTree().ChangeSceneToFile("res://Scenes/EndMenu.tscn");
 	}
 	
 	public override void _Process(double delta)
