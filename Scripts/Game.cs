60b9ca30ad5083e8b6e8529ff9cb8063ebc7d033
Nicholas Alexander Meert <alexmeert13@gmail.com>
2026-02-21 20:53:00 -0500

    Rotating Works Now

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -4,7 +4,9 @@ using System;
 public partial class Game : Node3D
 {
 	[Export] public Camera3D PlayerCam;
-	[Export] public Vector3 _zoomedPosition;
+	[Export] public Vector3 _zoomedPositionCenter = new Vector3(0.071f, 1.234f, 4.5f);
+	[Export] public Vector3 _zoomedPositionLeft = new Vector3(-1.4f, 1.256f, 6.34f);
+	[Export] public Vector3 _zoomedPositionRight = new Vector3(2.2f, 1.246f, 6.187f);
 	
 	private bool focusedOnMonitor = false;
 	private Vector3 _targetRotation;
@@ -24,23 +26,36 @@ public partial class Game : Node3D
 		PlayerCam.RotationDegrees = PlayerCam.RotationDegrees.Lerp(_targetRotation, 5f * (float)delta);
 		PlayerCam.Position = PlayerCam.Position.Lerp(_targetPosition, 5f * (float)delta);
 		
-		if(Input.IsActionJustPressed("Right"))
+		if(!focusedOnMonitor)
 		{
-			_targetRotation += new Vector3(0,-90,0);
+			if(Input.IsActionJustPressed("Right"))
+			{
+				_targetRotation += new Vector3(0,-90,0);
+			}
+			else if(Input.IsActionJustPressed("Left"))
+			{
+				_targetRotation += new Vector3(0,90,0);
+			}
 		}
-		else if(Input.IsActionJustPressed("Left"))
-		{
-			_targetRotation += new Vector3(0,90,0);
-		}
+
 		
 		if(Input.IsActionJustPressed("mouse_left") && !focusedOnMonitor)
 		{
+			GD.Print(_targetRotation.Y);
 			focusedOnMonitor = true;
-			_targetPosition = _zoomedPosition;
 			Input.MouseMode = Input.MouseModeEnum.Visible;
+			
+			if(_targetRotation.Y == -90)
+				_targetPosition = _zoomedPositionRight;
+			else if(_targetRotation.Y == 90)
+				_targetPosition = _zoomedPositionLeft;
+			else
+				_targetPosition = _zoomedPositionCenter;
 		}
+		
 		if(Input.IsActionJustPressed("mouse_right") && focusedOnMonitor)
 		{
+			GD.Print(_targetRotation.Y);
 			focusedOnMonitor = false;
 			_targetPosition = _defaultPosition;
 			Input.MouseMode = Input.MouseModeEnum.Captured;
