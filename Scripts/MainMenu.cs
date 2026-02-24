09e88591db5c8712feb37b0ea7256666f48cbd22
Nicholas Alexander Meert <alexmeert13@gmail.com>
2026-02-24 01:51:11 -0500

    Added Menu Music & Game Sounds

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -2,10 +2,13 @@ using Godot;
 
 public partial class MainMenu : Control
 {
+	[Export] AudioStreamPlayer _menuMusic;
+	
 	public override void _Ready()
 	{
 		GetNode<Button>("Panel/VBoxContainer/PlayButton").Pressed += OnPlay;
 		GetNode<Button>("Panel/VBoxContainer/QuitButton").Pressed += OnQuit;
+		_menuMusic.Play();
 	}
 
 	private void OnPlay()
